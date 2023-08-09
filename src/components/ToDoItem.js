f7aae3a0f1042f8ff0f154219bfde87471ed8daa
ozgeonline <nisroc.414@hotmail.com>
2023-08-09 21:08:57 +0300

    component tree

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ToDoItem(props){
+  // const [isDone,setIsDone] = useState(false);
+  // function handleClick(){
+  //   setIsDone(prevValue => !prevValue )
+  // }
+  return (
+  <div onClick={()=>{
+    props.onChecked(props.id)
+  }}>
+    {<li /* style={{textDecoration: isDone ? "line-through" : "none"}}*/>{props.text}</li> }
+  </div>
+  )
+}
+export default ToDoItem;
